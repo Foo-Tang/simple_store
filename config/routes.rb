0efb23bfe8d8035f79dc06bf166d9167fc75d329
Foo Tang <godfoo@gmail.com>
2023-09-25 19:27:28 -0500

    Updated files to display all products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :products, only: %i[index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
